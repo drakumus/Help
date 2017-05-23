28de7452608589704366800390f1b5d8c879f107
drakumus <rohancheeniyil@gmail.com>
2017-05-23 00:12:49 -0600

    Add project files.

---
+++ HelpCookies/Cookie.Designer.cs
@@ -0,0 +1,69 @@
+﻿namespace HelpCookies
+{
+    partial class Cookie
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.timeBar = new System.Windows.Forms.ProgressBar();
+            this.incBar = new System.Windows.Forms.ProgressBar();
+            this.SuspendLayout();
+            // 
+            // timeBar
+            // 
+            this.timeBar.Location = new System.Drawing.Point(12, 84);
+            this.timeBar.Name = "timeBar";
+            this.timeBar.Size = new System.Drawing.Size(618, 29);
+            this.timeBar.TabIndex = 0;
+            // 
+            // incBar
+            // 
+            this.incBar.Location = new System.Drawing.Point(12, 48);
+            this.incBar.Name = "incBar";
+            this.incBar.Size = new System.Drawing.Size(618, 29);
+            this.incBar.TabIndex = 1;
+            // 
+            // Cookie
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(642, 125);
+            this.Controls.Add(this.incBar);
+            this.Controls.Add(this.timeBar);
+            this.Name = "Cookie";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Cookie_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ProgressBar timeBar;
+        private System.Windows.Forms.ProgressBar incBar;
+    }
+}
+
---
+++ HelpCookies/Cookie.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HelpCookies
+{
+    public partial class Cookie : Form
+    {
+        private int time;
+
+        public Cookie()
+        {
+            time = 0;
+
+            InitializeComponent();
+        }
+
+        private void Cookie_Load(object sender, EventArgs e)
+        {
+
+            #region TimeUntilReset
+            DateTime currentTime = TimeZoneInfo.ConvertTimeBySystemTimeZoneId(
+    DateTime.UtcNow, "Eastern Standard Time");
+
+            double currentDay = 0;
+            double currentSeconds = currentTime.Second + (currentTime.Minute * 60) + (currentTime.Hour * 3600);
+            double progressToReset;
+
+            switch (currentTime.DayOfWeek)
+            {
+                case DayOfWeek.Monday:
+                    currentDay = 0;
+                    break;
+                case DayOfWeek.Tuesday:
+                    currentDay = 1;
+                    break;
+                case DayOfWeek.Wednesday:
+                    currentDay = 2;
+                    break;
+                case DayOfWeek.Thursday:
+                    currentDay = 3;
+                    break;
+                case DayOfWeek.Friday:
+                    currentDay = 4;
+                    break;
+                case DayOfWeek.Saturday:
+                    currentDay = 5;
+                    break;
+                case DayOfWeek.Sunday:
+                    currentDay = 6;
+                    break;
+            }
+
+            double percentOfDay = currentSeconds/86400;
+            progressToReset = 100 * (currentDay + percentOfDay) /7;
+
+            timeBar.Step = (int)progressToReset;
+            
+            //initialize values here
+            //this.timeBar.Step = 100;
+            this.timeBar.PerformStep();
+            #endregion
+
+            Timer timer = new Timer();
+            timer.Interval = 100;
+
+            timer.Tick += new EventHandler(OnTimedEvent);
+            timer.Start();
+        }
+
+        private void OnTimedEvent(Object source, EventArgs e)
+        {
+            if(time < 100)
+            {
+                incBar.Step = 1;
+                incBar.PerformStep();
+            }
+        } 
+    }
+}
